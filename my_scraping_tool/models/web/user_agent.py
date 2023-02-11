e3f80e3f70f73cd8e5541b8ed6b743456b0d8f04
koba-masa <kobayashi_mshr@koba-masa.com>
2023-02-11 13:15:30 +0900

    feat: UserAgent用のクラスを作成

---
+++ my_scraping_tool/models/web/user_agent.py
@@ -0,0 +1,8 @@
+class UserAgent:
+  @classmethod
+  def pc(cls):
+    return 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36'
+
+  @classmethod
+  def sp(cls):
+    return 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
---
+++ tests/models/web/test_user_agent.py
@@ -0,0 +1,10 @@
+import pytest
+import os
+
+from models.web.user_agent import UserAgent
+
+def test_pc():
+  assert UserAgent.pc() == 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36'
+
+def test_sp():
+  assert UserAgent.sp() == 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'

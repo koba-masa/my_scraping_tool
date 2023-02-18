e4fcad5face410ebbebd206ca9459334d368aeb1
koba-masa <kobayashi_mshr@koba-masa.com>
2023-02-18 22:32:08 +0900

    feat: 使用しなくなったモデルを削除

--- tests/models/test_url_list.py
+++ tests/models/test_url_list.py
@@ -3,17 +3,7 @@ import os
 
 from models.url_list import UrlList
 
-URL_LIST_TEST = 'tests/test_url_list.txt'
-
-def before():
-  with open(URL_LIST_TEST, mode='w') as f:
-    f.write("http://httpbin/status/200" + "\n")
-    f.write("http://httpbin/status/301" + "\n")
-
-def after():
-  os.remove(URL_LIST_TEST)
-
-before()
+URL_LIST_TEST = 'config/test/urls/for_url_list.txt'
 
 def test_init():
   assert type(UrlList(URL_LIST_TEST)) is UrlList
@@ -30,5 +20,3 @@ def test_read():
   results = UrlList(URL_LIST_TEST).read()
   assert type(results) is list
   assert "http://httpbin/status/301" in results
-
-#after()
